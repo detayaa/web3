9ebc51a7cddc58f075908f3b9fcd6634a2661b80
detayaa <drugochenko.taya@mail.ru>
2022-12-21 13:17:12 +0700

    1

---
+++ src/main/java/com/example/webrecipes/WebRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.example.webrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webrecipes/WebRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
